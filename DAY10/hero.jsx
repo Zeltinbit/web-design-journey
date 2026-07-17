2f6822f97be49e9963fc1083889693a7235b72b5
Zeltinbit <Tinbit23@gmail.com>
2026-07-17 22:04:46 +0300

    Start learning React fundamentals and create first React component

---
+++ DAY10/hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+    return (
+        <section>
+            <h1>Hello, I'm Tinbite Daniel</h1>
+            <p>
+                Future Full-Stack Developer
+            </p>
+        </section>
+    );
+}
+export default Hero;
